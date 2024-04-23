3993cafb4c6aa6c7f2729ff691ad645a0ef80f13
kendrafray <kendra.t.fray@gmail.com>
2024-04-22 20:54:43 -0400

    modal open and close

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,16 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+
+const profileEditCloseButton = document.querySelector("#profile-close-button");
+const profileEditCloseModal = document.querySelector("#profile-close-modal");
+
+profileEditCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
